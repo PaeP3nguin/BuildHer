b981f85b1e720758019f876af26effd5960574ea
paep3nguin <paep3nguin@gmail.com>
2017-04-07 03:04:47 -0500

    Remove bold effect on RadioButton press

--- app/src/main/java/com/wic/buildher/widget/BorderlessRadioButton.java
+++ app/src/main/java/com/wic/buildher/widget/BorderlessRadioButton.java
@@ -1,23 +1,16 @@
 package com.wic.buildher.widget;
 
 import android.content.Context;
-import android.content.res.TypedArray;
 import android.graphics.Paint;
-import android.graphics.Typeface;
 import android.util.AttributeSet;
 
 import com.wic.buildher.R;
-
-import uk.co.chrisjenx.calligraphy.CalligraphyUtils;
 
 /**
  * A borderless radio button that bolds and underlines its text when selected
  * Supports custom fonts when checked/unchecked
  */
 public class BorderlessRadioButton extends android.support.v7.widget.AppCompatRadioButton {
-    private String mCheckedFontPath;
-    private String mUncheckedFontPath;
-
     public BorderlessRadioButton(Context context) {
         this(context, null);
     }
@@ -28,11 +21,6 @@ public class BorderlessRadioButton extends android.support.v7.widget.AppCompatRa
 
     public BorderlessRadioButton(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, R.attr.BorderlessRadioButtonStyle);
-
-        TypedArray array = context.getTheme()
-                .obtainStyledAttributes(attrs, R.styleable.BorderlessRadioButton, 0, 0);
-        mCheckedFontPath = array.getString(R.styleable.BorderlessRadioButton_checkedFontPath);
-        mUncheckedFontPath = array.getString(R.styleable.BorderlessRadioButton_uncheckedFontPath);
     }
 
     @Override
@@ -41,18 +29,8 @@ public class BorderlessRadioButton extends android.support.v7.widget.AppCompatRa
 
         if (checked) {
             setPaintFlags(getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
-            if (mCheckedFontPath != null) {
-                CalligraphyUtils.applyFontToTextView(getContext(), this, mCheckedFontPath);
-            } else {
-                setTypeface(null, Typeface.BOLD);
-            }
         } else {
             setPaintFlags(getPaintFlags() & (~Paint.UNDERLINE_TEXT_FLAG));
-            if (mUncheckedFontPath != null) {
-                CalligraphyUtils.applyFontToTextView(getContext(), this, mUncheckedFontPath);
-            } else {
-                setTypeface(null, Typeface.NORMAL);
-            }
         }
     }
 }
